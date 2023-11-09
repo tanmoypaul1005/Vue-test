38c1f1274ec7cd6c11abb3f7e5ea880cc4b14a54
tanmoypaul1005 <tanmoypaul1005@gmail.com>
2023-11-09 19:18:10 +0600

    submitForm api implement

---
+++ src/components/SubmitForm.vue
@@ -0,0 +1,35 @@
+<template>
+  <form @submit="submitForm">
+    <label for="name">Name:</label>
+    <input type="text" id="name" v-model="formData.name" />
+
+    <label for="description">Description:</label>
+    <textarea id="description" v-model="formData.description"></textarea>
+
+    <button type="submit">Submit</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        name: '',
+        description: '',
+      },
+    };
+  },
+  methods: {
+  async submitForm() {
+    try {
+      const response = await axios.post('https://api.example.com/data', this.formData);
+      console.log('Data saved:', response.data);
+    } catch (error) {
+      console.error('Failed to save data:', error);
+    }
+  },
+},
+
+};
+</script>
